dc424b38ab6b1cf0a3e310c3347fe3131e161e7a
Nicholas Eras <nicoeras@hotmail.com>
2025-07-29 21:45:21 -0300

    fix prisma

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RecipesModule } from './recipes/recipes.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [RecipesModule],
+  imports: [RecipesModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- back/src/recipes/recipes.module.ts
+++ back/src/recipes/recipes.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RecipesController } from './recipes.controller';
 import { RecipesService } from './recipes.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [RecipesController],
   providers: [RecipesService]
 })
