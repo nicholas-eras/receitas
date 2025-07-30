9ba93f78a830f60746f6853993b6fb6356920f2e
Nicholas Eras <nicoeras@hotmail.com>
2025-07-29 21:40:07 -0300

    fix prisma

--- back/src/recipes/recipes.service.ts
+++ back/src/recipes/recipes.service.ts
@@ -1,27 +1,28 @@
 import { Injectable } from '@nestjs/common'
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class RecipesService {
+
+  constructor(private prisma: PrismaService){}
+
   findAll() {
-    return prisma.recipe.findMany({ orderBy: { createdAt: 'desc' } })
+    return this.prisma.recipe.findMany({ orderBy: { createdAt: 'desc' } })
   }
 
   findOne(id: string) {
-    return prisma.recipe.findUnique({ where: { id } })
+    return this.prisma.recipe.findUnique({ where: { id } })
   }
 
   create(data: any) {
-    return prisma.recipe.create({ data })
+    return this.prisma.recipe.create({ data })
   }
 
   update(id: string, data: any) {
-    return prisma.recipe.update({ where: { id }, data })
+    return this.prisma.recipe.update({ where: { id }, data })
   }
 
   delete(id: string) {
-    return prisma.recipe.delete({ where: { id } })
+    return this.prisma.recipe.delete({ where: { id } })
   }
 }
