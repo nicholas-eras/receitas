adb0c18e2eee5e621f27f89a4924fb9bbdc3f541
Nicholas Eras <nicoeras@hotmail.com>
2025-08-01 19:10:17 -0300

    fix import

--- back/src/recipes/recipes.module.ts
+++ back/src/recipes/recipes.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { RecipesController } from './recipes.controller';
 import { RecipesService } from './recipes.service';
 import { PrismaService } from '../prisma/prisma.service';
-import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
+import { CloudinaryModule } from '../cloudinary/cloudinary.module';
 
 @Module({
   imports: [CloudinaryModule],
